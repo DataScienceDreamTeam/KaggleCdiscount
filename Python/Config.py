8fda837a41a7adde4778db9ee108f7b12c6cd549
Gaël Superi <gael.superi@gstep.fr>
2017-09-28 10:10:29 +0200

    config

--- Python/Config.py
+++ Python/Config.py
@@ -3,7 +3,7 @@ import sys
 if sys.argv[1] == "VM":
     BSON_TRAIN_FILE = "/datadrive/KaggleCdiscount/train.bson"
     ENVIRONMENT = "VM"
-    MAX_PRODUCTS_TO_PROCESS = None
+    MAX_PRODUCTS_TO_PROCESS = 100000
 else:
     BSON_TRAIN_FILE = "C:\\Users\\gael.superi\\Documents\\GitHub\\KaggleCdiscount\\Data\\train.bson"
     ENVIRONMENT = "LAPTOP"
