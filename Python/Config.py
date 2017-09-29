f63bffd46d440938df6195761d921402a871fd3e
Gael Superi <gael.superi@gmail.com>
2017-09-29 06:58:59 +0200

    config desktop

--- Python/Config.py
+++ Python/Config.py
@@ -7,7 +7,16 @@ if sys.argv[1] == "VM":
     MAX_PRODUCTS_TO_PROCESS = 20000000
     MULTI_PROCESS_BATCH_SIZE = 200000
     MULTI_PROCESS_NB_ITER = 36
-else:
+elif sys.argv[1] == "DESKTOP":
+
+    BSON_TRAIN_FILE = "C:\\Users\\gael.superi\\Documents\\GitHub\\KaggleCdiscount\\Data\\train.bson"
+    ENVIRONMENT = "LAPTOP"
+    MAX_PRODUCTS_TO_PROCESS = 20000000
+    MULTI_PROCESS_BATCH_SIZE = 200000
+    MULTI_PROCESS_NB_ITER = 36
+    
+elif sys.argv[1] == "LAPTOP":
+
     BSON_TRAIN_FILE = "C:\\Users\\gael.superi\\Documents\\GitHub\\KaggleCdiscount\\Data\\train.bson"
     ENVIRONMENT = "LAPTOP"
     MAX_PRODUCTS_TO_PROCESS = 20000000
