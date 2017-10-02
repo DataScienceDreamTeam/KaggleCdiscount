4a62ec9c1813d0cb2f0119cd6544fdebc41860db
Gaël Superi <gael.superi@gstep.fr>
2017-10-02 13:57:57 +0200

    update

--- Python/TestLogisticRegressionScikit.py
+++ Python/TestLogisticRegressionScikit.py
@@ -9,11 +9,6 @@ from skimage.data import imread
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 
-#Test simple logistic regression with scikit learn (No GPU)
-#Cannot do it with all data on laptop because all the dataset has to be in RAM
-#Trying on laptop to see if the code works and then run it with the VM
-
-
 def readFileGetXY(fileName):
 
     print("start readFileGetXY %s" % fileName)
@@ -26,7 +21,6 @@ def readFileGetXY(fileName):
         product_id = d["_id"]
         category_id = d["category_id"]
 
-        print(product_id)
         for index_image, image in enumerate(d["imgs"]):
 
             picture = imread(io.BytesIO(image['picture']))
@@ -35,10 +29,22 @@ def readFileGetXY(fileName):
 
     return X,Y
 
-def save_model_parameters():
+def save_model_parameters(parameters):
 
 
     pass
+
+def get_accuracy(prediction, real_values):
+
+    nb_prediction_ok = 0
+    
+    for index, value in enumerate(prediction):
+        if real_values[index] == value:
+            nb_prediction_ok = nb_prediction_ok + 1
+
+    accuracy = nb_prediction_ok / len(prediction)
+
+    return accuracy
 
 def runLogisticRegression():
 
@@ -48,43 +54,27 @@ def runLogisticRegression():
     train_X_results,train_Y_categories = readFileGetXY(Config.BSON_SMALL_TRAIN_FILE)
     validation_X_results,validation_Y_categories = readFileGetXY(Config.BSON_SMALL_VALIDATION_FILE)
 
-
-    print(len(train_X_results))
-    print(len(train_Y_categories))
-
-    print(len(validation_X_results))
-    print(len(validation_Y_categories))
-    
+    print("Train file : nb items %i nb categories %i" % (len(train_X_results),len(train_Y_categories)))
+    print("Validation file : nb items %i nb categories %i" % (len(validation_X_results),len(validation_Y_categories)))
    
-    lb = preprocessing.LabelBinarizer()
-    print(lb.fit_transform(train_Y_categories))
-    print(lb.classes_)
-    print(lb.fit_transform(validation_Y_categories))
-    print(lb.classes_)
-    
-
     model = LogisticRegression()
     print("start fit")
     model.fit(train_X_results,train_Y_categories)
     print("end fit")
 
     prediction = model.predict(validation_X_results)
-    nb_prediction_ok = 0
-    
-    for index, value in enumerate(prediction):
-        if validation_Y_categories[index] == value:
-            nb_prediction_ok = nb_prediction_ok + 1
+    accuracy = get_accuracy(prediction, validation_Y_categories)
 
-    accuracy = nb_prediction_ok / len(prediction)
     print("accuracy = %f" % accuracy)
 
     model_parameters = model.get_params()
+    save_model_parameters(model_parameters)
     
     t2 = time.time()
-    print("end TestLogisticRegressionScikit")
+    
     total_time = t2 - t1
-    print(total_time)
-
+    print("Total time : %s" % str(total_time))
+    print("end TestLogisticRegressionScikit")
 
 
 runLogisticRegression()
